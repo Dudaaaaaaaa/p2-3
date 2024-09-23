f40fc68245f3d4fc85053ba294794671a6e45604
Xxduda <143605703+Dudaaaaaaaa@users.noreply.github.com>
2024-09-23 10:43:34 -0300

    sdg

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,22 @@
 criaCartao(
     'Donuts',
     'Como é feito:',
-    'O Python é uma linguagem de programação'
+    'Ingredientes:
+-4 xícaras de farinha de trigo
+-250 ml de leite integral
+-2 ovos
+-100 g de manteiga sem sal
+-4 colheres de sopa de açúcar
+-1 colher de chá de sal
+-10 g de fermento biológico seco
+-Baunilha a gosto
+Modo de Preparo:
+Misture os ingredientes: Em uma batedeira, adicione todos os ingredientes e misture até obter uma massa homogênea.
+Deixe a massa descansar: Cubra a massa com um pano e deixe descansar por cerca de 1 hora, até dobrar de tamanho.
+Modele os donuts: Abra a massa com um rolo até ficar com a espessura de um dedo. Corte no formato de donuts.
+Deixe descansar novamente: Coloque os donuts cortados em uma assadeira enfarinhada, cubra com um pano e deixe descansar por mais 15 minutos.
+Frite os donuts: Frite em óleo quente (160-170°C) até dourar. Escorra em papel toalha.
+Adicione a cobertura: Depois de esfriar, adicione a cobertura de sua preferência'
 )
 
 criaCartao(
@@ -17,7 +32,7 @@ criaCartao(
 )
 
 criaCartao(
-    'Lasanha',
+    'Torta',
     'Como é feito',
     'Oi em ingles é HI (RAI)'
 )
