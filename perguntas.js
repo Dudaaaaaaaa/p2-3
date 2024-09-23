f3b3175a808c9ac05b3b5463c3047eeb74efe44b
Xxduda <143605703+Dudaaaaaaaa@users.noreply.github.com>
2024-09-23 10:47:29 -0300

    j

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,7 @@
 criaCartao(
     'Donuts',
     'Como é feito:',
-    'Ingredientes:
--4 xícaras de farinha de trigo
--250 ml de leite integral
--2 ovos
--100 g de manteiga sem sal
--4 colheres de sopa de açúcar
--1 colher de chá de sal
--10 g de fermento biológico seco
--Baunilha a gosto
-Modo de Preparo:
-Misture os ingredientes: Em uma batedeira, adicione todos os ingredientes e misture até obter uma massa homogênea.
-Deixe a massa descansar: Cubra a massa com um pano e deixe descansar por cerca de 1 hora, até dobrar de tamanho.
-Modele os donuts: Abra a massa com um rolo até ficar com a espessura de um dedo. Corte no formato de donuts.
-Deixe descansar novamente: Coloque os donuts cortados em uma assadeira enfarinhada, cubra com um pano e deixe descansar por mais 15 minutos.
-Frite os donuts: Frite em óleo quente (160-170°C) até dourar. Escorra em papel toalha.
-Adicione a cobertura: Depois de esfriar, adicione a cobertura de sua preferência'
+    'Ingredientes: 4 xícaras de farinha de trigo/n250 ml de leite integral/n2 ovos'
 )
 
 criaCartao(
