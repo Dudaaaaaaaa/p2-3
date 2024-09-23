458480bdf0dbafc545b31188d33e3003ea49daac
Xxduda <143605703+Dudaaaaaaaa@users.noreply.github.com>
2024-09-23 10:57:15 -0300

    as

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Donuts',
-    'Como é feito:',
+    'Receita:',
     'Ingredientes: 4 xícaras de farinha de trigo<br>250 ml de leite integral<br>2 ovos100 g de manteiga sem sal<br>4 colheres de sopa de açúcar<br>1 colher de chá de sal<br>10 g de fermento biológico seco<br>Baunilha a gosto<br>Modo de Preparo:Misture os ingredientes: Em uma batedeira, adicione todos os ingredientes e misture até obter uma massa homogênea.Deixe a massa descansar: Cubra a massa com um pano e deixe descansar por cerca de 1 hora, até dobrar de tamanho.Modele os donuts: Abra a massa com um rolo até ficar com a espessura de um dedo. Corte no formato de donuts.Deixe descansar novamente: Coloque os donuts cortados em uma assadeira enfarinhada, cubra com um pano e deixe descansar por mais 15 minutos.Frite os donuts: Frite em óleo quente (160-170°C) até dourar. Escorra em papel toalha.Adicione a cobertura: Depois de esfriar, adicione a cobertura de sua preferência'
 )
 
 criaCartao(
     'Biscoito',
-    'Como é feito',
+    'Receita',
     'A capital da França é Paris'
 )
 
 criaCartao(
     'Pastel',
-    'Como é feito',
+    'Receita',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
     'Torta',
-    'Como é feito',
+    'Receita',
     'Oi em ingles é HI (RAI)'
 )
